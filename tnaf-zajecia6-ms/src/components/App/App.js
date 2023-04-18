69006bef04841d8a2fa10f313e8d9aaf1985980f
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-04-18 08:37:52 +0200

    2023041801

--- tnaf-zajecia6-ms/src/components/App/App.js
+++ tnaf-zajecia6-ms/src/components/App/App.js
@@ -1,5 +1,6 @@
 import Timer from '../Timer/Timer';
 import React from 'react';
+import './AppStyles.css';
 
 export default function App() {
   const [showTimer, setShowTimer] = React.useState(false);
@@ -7,11 +8,12 @@ export default function App() {
   const handleClick = () => setShowTimer(!showTimer);
 
   return (
-    <>
+    <div className={'appContainer'}>
       <button className={'button'} onClick={handleClick}>
         {showTimer ? 'Hide Timer' : 'Show Timer'}
       </button>
-      {showTimer && <Timer initialState={0} />}
-    </>
+      {showTimer && <Timer initialState={5} />}
+      <input></input>
+    </div>
   );
 }
--- tnaf-zajecia6-ms/src/components/Timer/Timer.jsx
+++ tnaf-zajecia6-ms/src/components/Timer/Timer.jsx
@@ -1,22 +1,49 @@
 import React, { useEffect } from 'react';
+import './TimerStyles.css';
+
+import img from '../../imgs/oogway.jpg';
 
 export default function Timer({ initialState }) {
   const [value, setValue] = React.useState(initialState);
+  const [running, setRunning] = React.useState(false);
+
+  const timerText = () => {
+    return value < 0 ? <img src={img} alt={'oogway'} /> : 'Time left: ' + value + 's';
+  };
+
+  const handleReset = () => {
+    setValue(initialState);
+    setRunning(false);
+  };
+
+  const handleStartStop = () => {
+    setRunning(!running);
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setValue(prevState => prevState + 1);
-      console.log('cyk');
-    }, 1000);
+    const interval = running
+      ? setInterval(() => {
+          setValue(prevState => prevState - 1);
+          console.log('cyk');
+        }, 1000)
+      : null;
+
+    if (value < 0) setRunning(false);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [value, running]);
 
   return (
-    <div>
-      <h2>Counting: {value}</h2>
+    <div className={'flexBox'}>
+      <h2>{timerText()}</h2>
+      <button className={'button'} onClick={handleStartStop}>
+        {running ? 'Stop' : 'Start'}
+      </button>
+      <button className={'button'} onClick={handleReset}>
+        Reset
+      </button>
     </div>
   );
 }
